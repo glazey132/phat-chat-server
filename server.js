88044385eefda463d04e6233c401aed2f64fdd43
glazey132 <alglaze132@gmail.com>
2019-08-13 16:41:51 -0700

    register route working

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 'use strict';
-
+/** 3rd party packages */
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator');
 
-const UserModel = require('./models/User');
+/** models */
+const User = require('./models/User');
 
+/** start server and connect to mongodb */
 const app = express();
+
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
 .then(() => {
     console.log('Mongoose is now connected!');
 })
 .catch(err => console.error(`There was an error: ${err}`));
 
-// create application/json parser
+/** create application/json parser */
 const jsonParser = bodyParser.json();
-// create application/x-www-form-urlencoded parser
+
+/** create application/x-www-form-urlencoded parser */
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
@@ -25,11 +30,37 @@ app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
-app.post('/register', jsonParser, (req, res) => {
-    console.log('req.body => ', req.body);
-    // const { username, password } = req.body;
-    // console.log("TCL: password", password);
-    // console.log("TCL: username", username);
-})
+app.post('/register', jsonParser, [
+    check('username')
+    .isAlpha()
+    .withMessage('Must be only alphabetical characters')
+    .isLength({ min: 3 })
+    .withMessage('Username must be at least 3 characters long'),
+    check('password')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+], (req, res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+    
+    // create and save new user
+    const newUser = new User(req.body);
+    newUser.save()
+    .then(result => {
+        console.log('result of save -> ', result);
+        res.status(200).json({ success: true, result});
+    })
+    .catch(err => {
+        console.error('There was an error when trying to save the new user: ', err);
+        res.status(500).json({ error: err })
+    })
+});
+
+app.post('/login', jsonParser, (req, res) => {
+
+});
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
